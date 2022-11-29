922b53f27f395416f65991e774237ef112172e84
brgcode <tom.v.mele@gmail.com>
2022-11-29 21:14:12 +0100

    pull up data structures

---
+++ src/compas_rv3/datastructures/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from .pattern import Pattern
+from .diagram import Diagram
+from .formdiagram import FormDiagram
+from .forcediagram import ForceDiagram
+from .thrustdiagram import ThrustDiagram
+
+__all__ = ["Pattern", "Diagram", "FormDiagram", "ForceDiagram", "ThrustDiagram"]
