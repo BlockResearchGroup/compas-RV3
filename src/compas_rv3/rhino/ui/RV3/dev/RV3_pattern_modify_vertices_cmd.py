140f570d7a5b827c3e7d5fa8def0d18bbd3a944e
brgcode <tom.v.mele@gmail.com>
2022-11-29 21:25:11 +0100

    pattern modify vertices command

--- src/compas_rv3/rhino/ui/RV3/dev/RV3_pattern_modify_vertices_cmd.py
+++ src/compas_rv3/rhino/ui/RV3/dev/RV3_pattern_modify_vertices_cmd.py
@@ -4,23 +4,22 @@ from __future__ import division
 
 import compas_rhino
 from compas.utilities import flatten
-
-# from compas_rv3.rhino import ModifyAttributesForm
+from compas_ui.ui import UI
 
 
 __commandname__ = "RV3_pattern_modify_vertices"
 
 
+@UI.error()
 def RunCommand(is_interactive):
 
-    scene = get_scene()
-    if not scene:
-        return
+    ui = UI()
 
-    pattern = scene.get("pattern")[0]
-    if not pattern:
-        print("There is no Pattern in the scene.")
+    objects = ui.scene.get("Pattern")
+    if not objects:
+        compas_rhino.display_message("There is no Pattern in the scene.")
         return
+    pattern = objects[0]
 
     options = ["AllBoundaryVertices", "Corners", "ByContinuousEdges", "Manual"]
 
@@ -30,78 +29,25 @@ def RunCommand(is_interactive):
         return
 
     if option == "AllBoundaryVertices":
-        keys = pattern.datastructure.vertices_on_boundary()
+        vertices = pattern.datastructure.vertices_on_boundary()
 
     elif option == "Corners":
-        angle = compas_rhino.rs.GetInteger(
-            "Angle tolerance for non-quad face corners:", 170, 1, 180
-        )
-        keys = pattern.datastructure.corner_vertices(tol=angle)
+        angle = compas_rhino.rs.GetInteger("Angle tolerance for non-quad face corners:", 170, 1, 180)
+        vertices = pattern.datastructure.corner_vertices(tol=angle)
 
     elif option == "ByContinuousEdges":
-        temp = pattern.select_edges()
-        keys = list(
-            set(
-                flatten(
-                    [pattern.datastructure.vertices_on_edge_loop(key) for key in temp]
-                )
-            )
-        )
-
-    # elif option == "ByConstraints":
-    #     guids = pattern.datastructure.vertices_attribute('constraints')
-    #     guids = list(set(list(flatten(list(filter(None, guids))))))
-
-    #     if not guids:
-    #         print('there are no constraints in this pattern')
-    #         return
-
-    #     current = pattern.settings['color.edges']
-    #     pattern.settings['color.edges'] = [120, 120, 120]
-    #     scene.update()
-
-    #     compas_rhino.rs.ShowObjects(guids)
-
-    #     def custom_filter(rhino_object, geometry, component_index):
-    #         if str(rhino_object.Id) in guids:
-    #             return True
-    #         return False
-
-    #     constraints = compas_rhino.rs.GetObjects('select constraints', custom_filter=custom_filter)
-
-    #     if not constraints:
-    #         return
-
-    #     keys = []
-    #     for guid in constraints:
-    #         for key, attr in pattern.datastructure.vertices(data=True):
-    #             if attr['constraints']:
-    #                 if str(guid) in attr['constraints']:
-    #                     keys.append(key)
-    #     keys = list(set(keys))
-
-    #     compas_rhino.rs.HideObjects(guids)
-    #     pattern.settings['color.edges'] = current
+        edges = pattern.select_edges()
+        vertices = list(set(flatten([pattern.datastructure.vertices_on_edge_loop(edge) for edge in edges])))
 
     elif option == "Manual":
-        keys = pattern.select_vertices()
+        vertices = pattern.select_vertices()
 
-    if keys:
-        # ModifyAttributesForm.from_sceneNode(pattern, 'vertices', keys)
-        # scene.update()
-        public = [
-            name
-            for name in pattern.datastructure.default_vertex_attributes.keys()
-            if not name.startswith("_")
-        ]
-        if pattern.update_vertices_attributes(keys, names=public):
-            scene.update()
+    if vertices:
+        public = [name for name in pattern.datastructure.default_vertex_attributes if not name.startswith("_")]
+        if pattern.update_vertices_attributes(vertices, names=public):
+            ui.scene.update()
+            ui.record()
 
-
-# ==============================================================================
-# Main
-# ==============================================================================
 
 if __name__ == "__main__":
-
     RunCommand(True)
