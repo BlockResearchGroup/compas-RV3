07b6e848e041157812801454bc104a13ba60ce4c
Licini <leochen1990@gmail.com>
2022-12-01 20:31:59 +0100

    post undo redo

--- src/compas_rv3/rhino/register.py
+++ src/compas_rv3/rhino/register.py
@@ -39,41 +39,16 @@ def pre_undo(ui):
     pass
 
 
-# @plugin(category="ui")
-# def post_undo(ui):
-#     pairs = {}
-
-#     for obj in ui.scene.objects:
-#         name = obj.name
-#         if name.startswith("FormDiagram"):
-#             if name == "FormDiagram":
-#                 index = 0
-#             else:
-#                 index = int(name.split(".")[-1]) + 1
-#             if index not in pairs:
-#                 pairs[index] = {"form": None, "force": None}
-#             pairs[index]["form"] = obj
-
-#     for obj in ui.scene.objects:
-#         name = obj.name
-#         if name.startswith("ForceDiagram"):
-#             if name == "ForceDiagram":
-#                 index = 0
-#             else:
-#                 index = int(name.split(".")[-1]) + 1
-#             if index not in pairs:
-#                 pairs[index] = {"form": None, "force": None}
-#             pairs[index]["force"] = obj
-
-#     for index in pairs:
-#         form = pairs[index]["form"]
-#         force = pairs[index]["force"]
-#         if form:
-#             if force:
-#                 form.diagram.dual = force.diagram
-#         if force:
-#             if form:
-#                 force.diagram.dual = form.diagram
+@plugin(category="ui")
+def post_undo(ui):
+    form = ui.scene.active_object.get_child_by_name("FormDiagram")
+    force = ui.scene.active_object.get_child_by_name("ForceDiagram")
+    if form:
+        if force:
+            form.diagram.dual = force.diagram
+    if force:
+        if form:
+            force.diagram.dual = form.diagram
 
 
 @plugin(category="ui")
@@ -81,38 +56,13 @@ def pre_redo(ui):
     pass
 
 
-# @plugin(category="ui")
-# def post_redo(ui):
-#     pairs = {}
-
-#     for obj in ui.scene.objects:
-#         name = obj.name
-#         if name.startswith("FormDiagram"):
-#             if name == "FormDiagram":
-#                 index = 0
-#             else:
-#                 index = int(name.split(".")[-1]) + 1
-#             if index not in pairs:
-#                 pairs[index] = {"form": None, "force": None}
-#             pairs[index]["form"] = obj
-
-#     for obj in ui.scene.objects:
-#         name = obj.name
-#         if name.startswith("ForceDiagram"):
-#             if name == "ForceDiagram":
-#                 index = 0
-#             else:
-#                 index = int(name.split(".")[-1]) + 1
-#             if index not in pairs:
-#                 pairs[index] = {"form": None, "force": None}
-#             pairs[index]["force"] = obj
-
-#     for index in pairs:
-#         form = pairs[index]["form"]
-#         force = pairs[index]["force"]
-#         if form:
-#             if force:
-#                 form.diagram.dual = force.diagram
-#         if force:
-#             if form:
-#                 force.diagram.dual = form.diagram
+@plugin(category="ui")
+def post_redo(ui):
+    form = ui.scene.active_object.get_child_by_name("FormDiagram")
+    force = ui.scene.active_object.get_child_by_name("ForceDiagram")
+    if form:
+        if force:
+            form.diagram.dual = force.diagram
+    if force:
+        if form:
+            force.diagram.dual = form.diagram
