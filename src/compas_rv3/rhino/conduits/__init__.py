382ae64e060abb5a655a7505cd70c5ff2f96ef15
brgcode <tom.v.mele@gmail.com>
2022-12-01 20:28:05 +0100

    remove force conduit

--- src/compas_rv3/rhino/conduits/__init__.py
+++ src/compas_rv3/rhino/conduits/__init__.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from .forceconduit import ForceConduit  # noqa: F401 F403
 from .horizontalconduit import HorizontalConduit  # noqa: F401 F403
 from .subdconduit import SubdConduit  # noqa: F401 F403
 from .thrustconduit import SelfWeightConduit  # noqa: F401 F403
