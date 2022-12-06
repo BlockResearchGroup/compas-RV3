960f6318fb32b90e1513f3e78d7f14906864d57e
brgcode <tom.v.mele@gmail.com>
2022-12-06 07:56:18 +0100

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_rv3",
-    version="0.3.0",
+    version="0.4.0",
     description="Funicular form finding based on compas_tna",
     long_description=long_description,
     long_description_content_type="text/markdown",
